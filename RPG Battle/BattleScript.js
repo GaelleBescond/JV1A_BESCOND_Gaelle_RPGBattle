61e483240a995444be9e925133f6e26d3a0adc11
GaelleBescond <118370404+GaelleBescond@users.noreply.github.com>
2022-12-02 11:50:09 +0100

    Prepa avant cours

--- RPG Battle/BattleScript.js
+++ RPG Battle/BattleScript.js
@@ -2,50 +2,59 @@
 //}, timeout);
 
 
+DivYann = document.getElementById("DivYann");
+HPYann = document.getElementById("HPYann");
+DivBaptiste = document.getElementById("DivBaptiste");
+HPBaptiste = document.getElementById("HPBaptiste");
+DivGuenole = document.getElementById("DivGuenole");
+HPGuenole = document.getElementById("HPGuenole");
+DivMarie = document.getElementById("DivMarie");
+HPMarie = document.getElementById("HPMarie");
+DivMonstre1 = document.getElementById("DivMonstre1");
+HPMonstre1 = document.getElementById("HPMonstre1");
 
-    afficheAction = document.getElementById("afficheAction");
-    afficheAction.innerHTML = "Le combat commence!";
 
-    
-//Actualisation Dialogue
-afficheAction.onclick = function() {
-    afficheAction.innerHTML = "Sélectionnez un personnage, puis l'action qu'il doit effectuer.";
-}
+afficheAction = document.getElementById("afficheAction");
+afficheAction.innerHTML = "Le combat commence!";
 
+
+/*A faire:
+- Riposte du monstre
+- Complétion de toutes les attaques pour riposte
+- 3 monstres
+- attaques aléatoires monstres
+- Mort des personnages (hp<=0)
+- Affichage pv monstres sur hover
+- Mort des monstres
+- Sélection personnage onclick
+- Affichage menu de choix selon perso
+- Coût mana pouvoir + lock réutlisation (mana interne à 1?)
+*/
 
 //Personnages
     //Personnage1: Yann
-    DivYann = document.getElementById("DivYann");
-    HPYann = document.getElementById("HPYann");
     DivYann.onclick = function() {
         afficheAction.innerHTML = "Vous sélectionnez Yann";
+        Attaque()
         }
 
     //Personnage2: Baptiste
-    DivBaptiste = document.getElementById("DivBaptiste");
-    HPBaptiste = document.getElementById("HPBaptiste");
     DivBaptiste.onclick = function() {
         afficheAction.innerHTML = "Vous sélectionnez Baptiste";
         }
 
     //Personnage3: Guenole
-    DivGuenole = document.getElementById("DivGuenole");
-    HPGuenole = document.getElementById("HPGuenole");
     DivGuenole.onclick = function() {
         afficheAction.innerHTML = "Vous sélectionnez Guenole";
         }
 
     //Personnage4: Marie
-    DivMarie = document.getElementById("DivMarie");
-    HPMarie = document.getElementById("HPMarie");
     DivMarie.onclick = function() {
         afficheAction.innerHTML = "Vous sélectionnez Marie";
         }
 
 //Monstres
     //Monstre1 
-    DivMonstre1 = document.getElementById("DivMonstre1");
-    HPMonstre1 = document.getElementById("HPMonstre1");
 
 //Actions possibles
     //Action attaque
